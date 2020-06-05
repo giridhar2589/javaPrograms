11a52f00515b97a542e258c549aa39723c82b7be
Giridhar <giridharkc0@gmail.com>
2020-06-05 22:08:45 +0530

    Frist commit:Frist String Classes

--- src/test/java/programs/Strings.java
+++ src/test/java/programs/Strings.java
@@ -2,6 +2,15 @@ package programs;
 
 public class Strings {
 
-	
-	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String a = "Java String";
+		System.out.println(a.charAt(2));
+		System.out.println(a.indexOf("t"));
+		
+		
+	}
+
 }
